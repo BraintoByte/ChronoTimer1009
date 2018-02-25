87b5ca0b6796c7a18bed240f3c491af5326ba13d
Brain to Bytes <gaspari4@uwm.edu>
2018-02-24 20:23:02 -0600

    Please luis review this and the simulator class

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -6,13 +6,5 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		
-		Simulator sim = new Simulator();
-		try {
-			sim.start();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 	}
 }
