3c1641a49a6ed39944e12513f3690a17f26c1f7f
gLuis462 <Gonza462@uwm.edu>
2018-02-24 18:46:20 -0600

    push

--- src/Utils/Util.java
+++ src/Utils/Util.java
@@ -1,5 +1,28 @@
 package Utils;
 
-public class Util {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Util {
+	
+	/**
+	   * Opens and reads a file, and returns the contents as one String.
+	   */
+	  public static String readFileAsString(String filename) 
+	  throws IOException
+	  {
+	    BufferedReader reader = new BufferedReader(new FileReader(filename));
+	    String line;
+	    StringBuilder sb = new StringBuilder();
+	    while ((line = reader.readLine()) != null)
+	    {
+	      sb.append(line + "\n");
+	    }
+	    reader.close();
+	    return sb.toString();
+	  }
+
+	
 }
+	  
