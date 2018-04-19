7d897da414c87ba263c22e4efd9fcd91e1ffa071
Brain to Bytes <gaspari4@uwm.edu>
2018-04-19 12:45:06 -0600

    Up to date!

--- src/testing/TestSuite.java
+++ src/testing/TestSuite.java
@@ -1,9 +1,5 @@
 package testing;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
